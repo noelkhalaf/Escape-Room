020a38653f74828c9aeea2ab4b83c4736db02a53
noelkhalaf <noel_khalaf@hotmail.com>
2023-11-18 20:44:46 -0500

    Aligned scenes and added vault

--- src/vault/vault.gd
+++ src/vault/vault.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
-var cabinet_closed_t = preload("res://assets/images/cabinet.png")
-var cabinet_open_t = preload("res://assets/images/cabinet-open.png")
+var vault_closed_t = preload("res://assets/images/vault.png")
+var vault_open_t = preload("res://assets/images/vault-open.png")
 
-@onready var cabinet_s = $CabinetImage
+@onready var vault_s = $VaultImage
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,7 +16,7 @@ func _process(delta):
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		if cabinet_s.get_texture() == cabinet_closed_t:
-			cabinet_s.set_texture(cabinet_open_t)
+		if vault_s.get_texture() == vault_closed_t:
+			vault_s.set_texture(vault_open_t)
 		else:
-			cabinet_s.set_texture(cabinet_closed_t)
+			vault_s.set_texture(vault_closed_t)
