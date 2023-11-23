ad8e0cd5186f20e6ff25d986fb5ee480b93b4cc6
noelkhalaf <noel_khalaf@hotmail.com>
2023-11-22 22:40:11 -0500

    Added correct passcode keypad functionality

--- src/vault/keypad/keypad.gd
+++ src/vault/keypad/keypad.gd
@@ -1,7 +1,22 @@
 extends Area2D
 
-@onready var back_s = $Back
 @onready var vault_s = $".."
+@onready var back_s = $Back
+@onready var enter_s = $Enter
+@onready var clear_s = $Clear
+@onready var zero_s = $Zero
+@onready var one_s = $One
+@onready var two_s = $Two
+@onready var three_s = $Three
+@onready var four_s = $Four
+@onready var five_s = $Five
+@onready var six_s = $Six
+#@onready var seven_s = $Seven
+#@onready var eight_s = $Eight
+#@onready var nine_s = $Nine
+
+const correct_passcode: String = "3206"
+var user_passcode: String = ""
 
 # Called when the node enters the scene tree for the first time.
 func ready():
@@ -19,9 +34,22 @@ func _input_event(viewport, event, shape_idx):
 				back_clicked()
 			1:
 				enter_clicked()
+			2:
+				clear_user_passcode()
+			_:
+				number_clicked(shape_idx - 3)
 
 func back_clicked():
 	self.visible = false
 
 func enter_clicked():
-	vault_s.unlock_vault() # TODO check if combination is correct
+	if user_passcode == correct_passcode:
+		vault_s.unlock_vault()
+	else:
+		clear_user_passcode()
+
+func clear_user_passcode():
+	user_passcode = ""
+
+func number_clicked(number):
+	user_passcode = user_passcode + str(number)
